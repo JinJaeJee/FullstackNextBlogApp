82b6c92c96926b35f60c298f74a91dce99ee4087
JinJaeJee <jpluemphiromnat@gmail.com>
2023-09-13 20:37:01 +0700

    feat: initial theme toggle

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './themetoggle.module.css'
+import Image from 'next/image'
 
 const ThemeToggle = () => {
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div className={styles.container}>
+      <Image src="/moon.png" alt='moon' width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt='sun' width={14} height={14} />
+    </div>
   )
 }
 
