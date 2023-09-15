c378b1f2e61ca1deb6983c696df3f24698bc854f
JinJaeJee <jpluemphiromnat@gmail.com>
2023-09-15 18:52:06 +0700

    feat: usecontext api V1

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,20 +1,24 @@
+"use client";
+
 import { createContext, useState } from "react";
 
 export const ThemeContext = createContext()
 
 const getFromLocalStorage = ()=>{
     if(typeof window !== undefined){
-        const value = localStorage.getItem("theme")
-        return value || "light"
+        const value = localStorage.getItem("theme");
+        return value || "light";
     }
 }
 
 export const ThemeContextProvider = ({children})=>{
 
-    const [Theme, setTheme] = useState(()=>{
+    const [theme, setTheme] = useState(()=>{
         return getFromLocalStorage()
     })
 
-
-    return <ThemeContext.Provider>{children}</ThemeContext.Provider>
+    const toggle = () =>{
+        setTheme(theme=="light" ? "dark" : "light")
+    }
+    return <ThemeContext.Provider value={{ theme, toggle }}>{children}</ThemeContext.Provider>
 }
