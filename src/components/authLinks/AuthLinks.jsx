7284ca7f4a691d42baeb898bb4b39a6867a94ad0
JinJaeJee <jpluemphiromnat@gmail.com>
2023-09-16 14:08:57 +0700

    feat: burger V.1 responsive for small devices

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,23 +1,44 @@
+"use client"
 import React from 'react'
 import styles from './authlink.module.css'
 import Link from 'next/link'
+import  { useState } from 'react';
 
 const AuthLinks = () => {
 
-  ///temporaly used
+  const [open, setOpen] = useState(false)
   const status = "notauthentification"
 
   return (
     <>
       {status==="notauthentification" ? (
-        <Link href="/login">Login</Link>) : 
+        <Link href="/login" className={styles.link}>Login</Link>) : 
+        (
+          <>
+            <Link href="/write" className={styles.link}>Write</Link>
+            <span className={styles.link}>Logout</span>
+          </>        
+        )}
+        <div className={styles.burger} onClick={() => setOpen(!open)}>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+        </div>
+        {open && (
+          <div className={styles.responsiveMenu}>
+            <Link href="/">Homepage</Link>
+            <Link href="/">Homepage</Link>
+            <Link href="/">Homepage</Link>
+            {status==="notauthentification" ? (
+              <Link href="/login">Login</Link>) : 
         (
           <>
             <Link href="/write">Write</Link>
-            <span>Logout</span>
+            <span className={styles.link}>Logout</span>
           </>        
-        )
-      }
+        )}
+          </div>
+        )}
     </>
   )
 }
