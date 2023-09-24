90e1be8b49b0bc6d539cad0ecaf1b405839d07a8
JinJaeJee <jpluemphiromnat@gmail.com>
2023-09-24 20:30:19 +0700

    feat: roubter page

--- src/components/memucategory/MenuCategory.jsx
+++ src/components/memucategory/MenuCategory.jsx
@@ -8,19 +8,19 @@ const MenuCategory = () => {
           <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.style}`}>
               Style
           </Link>
-          <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.fashion}`}>
+          <Link href="/blog" className={`${styles.categoryItem} ${styles.fashion}`}>
               Fashion
           </Link>
-          <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.food}`}>
+          <Link href="/blog" className={`${styles.categoryItem} ${styles.food}`}>
               Food
           </Link>
-          <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.culture}`}>
+          <Link href="/blog" className={`${styles.categoryItem} ${styles.culture}`}>
               Culture
           </Link>
-          <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.coding}`}>
+          <Link href="/blog" className={`${styles.categoryItem} ${styles.coding}`}>
               Coding
           </Link>
-          <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.travel}`}>
+          <Link href="/blog" className={`${styles.categoryItem} ${styles.travel}`}>
               Travel
           </Link>
       </div>
