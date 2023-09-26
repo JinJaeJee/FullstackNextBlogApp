0f7b15078dbd531b3f6fd776ce2ba7c262d2ef23
JinJaeJee <jpluemphiromnat@gmail.com>
2023-09-26 16:59:00 +0700

    feat: single page v1

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import styles from "./comments.module.css"
 import Link from "next/link"
 
@@ -13,7 +14,25 @@ const Comments = () => {
 
             </div>
         ) : (
-            <Link href="/login"> login to write a comment </Link>)}
+            <Link href="/login"> login to write a comment </Link>
+            )}
+            <div className={styles.comments}>
+              <div className={styles.comment}>
+                <div className={styles.user}>
+                  <Image src="/p1.jpeg" alt="" width={50} height={50} className={styles.img}/>
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}> John Dodo </span>
+                    <span className={styles.date}> 01.01.2023 </span>
+                  </div>
+                </div>
+                <p className={styles.decs}>
+                  It is a long established fact that a reader will be distracted by the
+                  readable content of a page when looking at its layout. The point of
+                  using Lorem Ipsum is that it has a more-or-less normal distribution
+                  of letters, as opposed to using Content here, content here making it.
+                </p>
+              </div>
+            </div>
     </div>
   )
 }
