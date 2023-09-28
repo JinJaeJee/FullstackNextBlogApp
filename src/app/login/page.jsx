8628aeb3f465df36bea2bacc3c2ee832a4372d92
JinJaeJee <jpluemphiromnat@gmail.com>
2023-09-28 18:43:27 +0700

    feat: prisma v1 oauth

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,20 @@
 "use client"
 import { signIn, useSession } from 'next-auth/react'
 import styles from './login.module.css'
+import { useRouter } from 'next/navigation'
 
 const Loginpage = () => {
 
   const { data, status } = useSession()
-  console.log(data,status)
+  const router = useRouter()
+
+  if (status === "loading"){
+    return <div className={styles.loading}> Loading... </div>
+  }
+
+  if (status === "authenticated"){
+    router.push("/")
+  }
 
   return (
     <div className={styles.container}>
--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,20 +3,22 @@ import React from 'react'
 import styles from './authlink.module.css'
 import Link from 'next/link'
 import  { useState } from 'react';
+import { signOut, useSession } from 'next-auth/react';
 
 const AuthLinks = () => {
 
   const [open, setOpen] = useState(false)
-  const status = "authentification"
+
+  const { status } = useSession()
 
   return (
     <>
-      {status==="notauthentification" ? (
+      {status=== "unauthenticated" ? (
         <Link href="/login" className={styles.link}>Login</Link>) : 
         (
           <>
             <Link href="/write" className={styles.link}>Write</Link>
-            <span className={styles.link}>Logout</span>
+            <span className={styles.link} onClick={signOut}>Logout</span>
           </>        
         )}
         <div className={styles.burger} onClick={() => setOpen(!open)}>
