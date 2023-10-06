75f558f67025d6060c23841549553bd031026fa2
JinJaeJee <jpluemphiromnat@gmail.com>
2023-10-06 22:03:09 +0700

    feat: comment section V.1

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth"
 import prisma from "@/utils/connect"
 import { NextResponse } from "next/server"
 
@@ -16,6 +17,28 @@ export const GET = async (req)=>{
             include: { user : true } 
     });
         return new NextResponse(JSON.stringify(comments , { status: 200 }));
+    }catch(err){
+        console.log(err)
+        return new NextResponse(JSON.stringify({message:"Something went wrong!!!"}, {status:500}))
+    }
+}
+
+
+//////Create a comment
+export const POST = async (req)=>{
+
+    const session = await getAuthSession()
+
+    if(!session){
+        return new NextResponse(JSON.stringify({message:"YOU ARE NOT AUTHENTICATED!!!"}, {status:401}))
+    }
+    
+    try{
+        const body = await req.json()
+        const comment = await prisma.comment.create({
+            data:{ ...body, userEmail:session.user.email },
+    });
+        return new NextResponse(JSON.stringify(comment , { status: 200 }));
     }catch(err){
         console.log(err)
         return new NextResponse(JSON.stringify({message:"Something went wrong!!!"}, {status:500}))
